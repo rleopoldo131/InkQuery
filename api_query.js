ab62326e1fa7e03184d7da9289060b1ced263dce
ethanseyalioglu <seyacocuk@gmail.com>
2024-10-18 16:28:20 -0700

    API get - Input desired word in command line and get responses from API (Merriam Webster Intermediate) in JSON format.

--- api_query.js
+++ api_query.js
@@ -1,14 +1,14 @@
 
 // Access command line arguments
-const word = process.argv.slice(2); // Skip the first two elements
+const word = process.argv.slice(2)[0]; // Skip the first two elements
 
 // args will now contain only the arguments passed to the script
-console.log('Word to search:', word[0]);
+console.log('Word to search:', word);
 
 const axios = require('axios');
 
 const wb_intermediate_key = "a19c9426-51cf-4c68-af55-de3b8229f559";
-const url = `https://dictionaryapi.com/api/v3/references/sd3/json/test?key=${wb_intermediate_key}`;
+const url = `https://dictionaryapi.com/api/v3/references/sd3/json/${word}?key=${wb_intermediate_key}`;
 
 console.log(url)
 
@@ -19,6 +19,5 @@ axios.get(url)
     .catch(error => {
         console.error('Error fetching the definition:', error);
     });
-
 
 
